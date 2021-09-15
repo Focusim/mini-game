59cdf7248ed21e5c960cf89359dbf518166fe484
Focusim <88191422+Focusim@users.noreply.github.com>
2021-09-15 21:06:56 +0300

    new

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
 top = 0
 left = 0
 
+let winWidth = document.documentElement.clientWidth
+let winHeight = document.documentElement.clientHeight
+console.log("Ширина:" + winWidth + " Высота:" + winHeight)
 
-document.addEventListener('keydown', function (event) {
+document.addEventListener('keydown', function(event) {
     if (event.code == "ArrowLeft" && (event.ctrlKey || event.metaKey)) {
-        left -= 10
+        if (left >= 100) {
+            left -= 100
+        }
         document.getElementById("item").style.left = left + "px"
     }
     if (event.code == "ArrowRight" && (event.ctrlKey || event.metaKey)) {
-        left += 10
+        if (left <= 1000) {
+            left += 100
+        }
         document.getElementById("item").style.left = left + "px"
     }
     if (event.code == "ArrowUp" && (event.ctrlKey || event.metaKey)) {
-        top -= 10
-        document.getElementById("item").style.left = top + "px"
+        if (bott >= 100) {
+            bott -= 100
+        }
+        document.getElementById("item").style.top = bott + "px"
     }
     if (event.code == "ArrowDown" && (event.ctrlKey || event.metaKey)) {
-        top += 10
-        document.getElementById("item").style.top = top + "px"
+        if (bott <= 100) {
+            bott -= 100
+        }
+        document.getElementById("item").style.top = bott + "px"
     }
 })
