870c1f92877950636f892bbb31534845d2ca4ee9
Focusim <88191422+Focusim@users.noreply.github.com>
2021-09-20 03:39:53 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-var bott = 0
+
+var topp = 0
 var left = 0
+
 var itemWidth = document.getElementById('item').offsetWidth
 var itemHeight = document.getElementById('item').offsetHeight
+
 var boxWidth = document.getElementById('box').offsetWidth
 var boxHeight = document.getElementById('box').offsetHeight
+
 var winWidth = document.documentElement.clientWidth -= itemWidth
 var winHeight = document.documentElement.clientHeight -= itemHeight
 
-console.log("Ширина:" + winWidth + " Высота:" + winHeight)
+console.log("Ширина:" + winWidth + " Высота:" + winHeight) 
 
 document.addEventListener('keydown', function (event) {
-    if (event.code == "ArrowLeft" && (event.ctrlKey || event.metaKey)) {
+    if (event.code == "ArrowLeft") {
         if (left >= 100) {
             left -= 100
             document.getElementById("item").style.right = null
         }
         document.getElementById("item").style.left = left + "px"
     }
-    if (event.code == "ArrowRight" && (event.ctrlKey || event.metaKey)) {
+    if (event.code == "ArrowRight") {
         if (left <= winWidth) {
             left += 100
             document.getElementById("item").style.left = left + "px"
@@ -29,26 +33,29 @@ document.addEventListener('keydown', function (event) {
     }
 
 
-    if (event.code == "ArrowUp" && (event.ctrlKey || event.metaKey)) {
-        if (bott >= 100) {
-            bott -= 100
+    if (event.code == "ArrowUp") {
+        if (topp >= 100) {
+            topp -= 100
             document.getElementById("item").style.bottom = null
-
         }
-        document.getElementById("item").style.top = bott + "px"
+        document.getElementById("item").style.top = topp + "px"
     }
-    if (event.code == "ArrowDown" && (event.ctrlKey || event.metaKey)) {
-        if (bott <= winHeight) {
-            bott += 100
-            document.getElementById("item").style.top = bott + "px"
+    if (event.code == "ArrowDown") {
+        if (topp <= winHeight) {
+            topp += 100
+            document.getElementById("item").style.top = topp + "px"
         }
     }
-    if (bott > winHeight) {
+    if (topp > winHeight) {
         document.getElementById("item").style.top = null
         document.getElementById("item").style.bottom = "0px"
     }
+    test1()
+    test2()
+    test()
+    return
 })
-
+/////////////////////////////////////////////////////////////
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
@@ -57,80 +64,92 @@ var randomWidth = getRandomInt(0, winWidth)
 document.getElementById('box').style.left = randomWidth + "px"
 var randomHeight = getRandomInt(0, winHeight)
 document.getElementById('box').style.top = randomHeight + "px"
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+///console.log(arrBoxX)
+///console.log("Стартовая точка по оси X: " + randomWidth)
+///console.log("Конечная точка по оси X: " + lengthBoxX)
+///console.log(arrBoxY)
+///console.log("Стартовая точка по оси Y: " + randomHeight)
+////-console.log("Конечная точка по оси Y: " + lengthBoxY)
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 
+var arrItemX = []
+var arrItemY = []
 
+function test1() {
+    checkLeft = item.getBoundingClientRect().x
+    var lengthItemX = checkLeft + itemWidth
+    for (i = checkLeft; i <= lengthItemX; i++) {
+        arrItemX.push(i)
+    }
+    arrItemX.forEach(function (item, index, array) {
+        arrLengthItemX = item
+    })
 
-
-
-
-
-
-var lengthBoxX = randomWidth + boxWidth
+    checkTop = item.getBoundingClientRect().y
+    var lengthItemY = checkTop + itemHeight
+    for (i = checkTop; i <= lengthItemY; i++) {
+        arrItemY.push(i)
+    }
+    arrItemY.forEach(function (item, index, array) {
+        arrLengthItemY = item
+    })
+}
 
 var arrBoxX = []
-for (i = randomWidth; i <= lengthBoxX; i++) {
-    arrBoxX.push(i)
-}
-
-/*вычислил ширину блока по значению left*/
-arrBoxX.forEach(function (item, index, array) {
-    arrLengthBoxX = item
-})
-
-console.log(arrBoxX)
-console.log("Стартовая точка по оси X: " + randomWidth)
-console.log("Конечная точка по оси X: " + lengthBoxX)
-
-
-var lengthBoxY = randomHeight + boxHeight
-
 var arrBoxY = []
-for (i = randomHeight; i <= lengthBoxY; i++) {
-    arrBoxY.push(i)
+
+function test2() {
+    var lengthBoxX = randomWidth + boxWidth
+    for (i = randomWidth; i <= lengthBoxX; i++) {
+        arrBoxX.push(i)
+    }
+    arrBoxX.forEach(function (item, index, array) {
+        arrLengthBoxX = item
+    })
+
+
+    var lengthBoxY = randomHeight + boxHeight
+    for (i = randomHeight; i <= lengthBoxY; i++) {
+        arrBoxY.push(i)
+    }
+    arrBoxY.forEach(function (item, index, array) {
+        arrLengthBoxY = item
+    })
 }
 
-/*вычислил высоту блока по значению top*/
-arrBoxY.forEach(function (item, index, array) {
-    arrLengthBoxY = item
-})
+var next = 0
 
-console.log(arrBoxY)
-console.log("Стартовая точка по оси Y: " + randomHeight)
-console.log("Конечная точка по оси Y: " + lengthBoxY)
-
-
-a = [1,2,3,5]
-b = [5,6,7,8,4]
+function test() {
+    var a = arrBoxX
+    var b = arrBoxY
+    var c = arrItemX
+    var d = arrItemY
+    
 
 
-
-
-
-if (500 === arrBoxX) {
-    alert("yes")
-} else {
-    alert("no")
+    for (let j = 0; j < itemWidth; j++) {
+        for (let i = 0; i < boxWidth; i++) {
+            if (c[j] == a[i]) {
+                var next = 1
+                console.log("ось X совпадает")
+                break
+            }
+            break
+        }
+    }
+    if (next === 1) {
+        for (let j = 0; j < itemHeight; j++) {
+            for (let i = 0; i < boxHeight; i++) {
+                if (d[j] == b[i]) {
+                    document.getElementById('item').style.display = "none"
+                    console.log("yes")
+                    break
+                }
+                break
+            }
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
